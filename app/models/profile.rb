b64ea247526dd5b20d90ef919d7bd4c5e0ef3d05
OnlySteveH <steve.hunter@bigdogconsultants.co.uk>
2016-06-13 19:55:02 +0100

    minor change to check pull requests work

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -4,6 +4,8 @@ class Profile < ActiveRecord::Base
   has_many :capsules
   before_save :profile_name
 
+  # Steve H added a comment just to make a change
+
 
     def profile_name
       @profile_name = "#{first_name} #{last_name}"
